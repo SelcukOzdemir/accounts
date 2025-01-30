a7d864de62d94e5189fa914f0d1406e04a8555c4
Selçuk Özdemir <selcuk.ozdemir@obss.com.tr>
2025-01-30 16:55:23 +0300

    Microservice of Accounts

---
+++ accounts/src/test/java/com/sso/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
